dfb6402faac6f788015a386ad85af361b5e15ae4
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-09-06 06:01:29 +0200

    Use GitHub API in contributions.js

--- assets/js/contributions.js
+++ assets/js/contributions.js
@@ -1,10 +1,35 @@
-fetch(
-  "https://github-contributions-api.now.sh/v1/YoussefRaafatNasry?format=nested"
-)
+---
+---
+
+var githubTopRepo = "lauripiispanen/github-top";
+var egyptPath = "_data/locations/egypt.yml";
+var login = "{{ site.github.owner.login }}"
+
+fetch(`https://api.github.com/repos/${githubTopRepo}/contents/${egyptPath}`)
   .then(response => response.json())
   .then(data => {
-    var total = 0;
-    Object.keys(data.years).forEach(year => (total += data.years[year].total));
-    var el = document.getElementById("contrib-count");
-    el.innerText = total;
+    var me = atob(data.content)
+      .split("\n\n")
+      .find(u => u.includes(login));
+    var json = userToJson(me);
+
+    var el1 = document.getElementById("contrib-count");
+    el1.innerText = json.contributions;
+
+    var el2 = document.getElementById("gh-rank");
+    el2.innerText = `Ranked the ${getNumberWithOrdinal(json.rank)} GitHub User in Egypt`;
   });
+
+function userToJson(user) {
+  var formatted = user
+    .replace(/  - |    |'/g, '"') // Replace line beginnings/single quotes with double quotes
+    .replace(/: /g, '": ') // Replace colons with double quotes
+    .replace(/\n/g, ", "); // Replace newlines with commas
+  return JSON.parse(`{ ${formatted}} `);
+}
+
+function getNumberWithOrdinal(n) {
+  var s = ["th", "st", "nd", "rd"];
+  var v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
