42f615b58b5d6a8e011204b51e45e8e1b31cc218
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-02-17 04:30:03 +0200

    V2 Initial Commit

--- assets/js/preshow.js
+++
@@ -1,6 +0,0 @@
-let projects = document.getElementsByClassName("project");
-setTimeout(function () {
-  for (var i = 0; i < projects.length; i++) {
-    projects[i].classList.remove("preShow")
-  }
-}, 2000);
