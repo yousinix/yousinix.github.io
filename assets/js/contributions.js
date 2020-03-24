126892e306023cda1b38b4788e7884390a8f3cfa
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-03-24 07:56:13 +0200

    Fix Fetching Contributions Bug

--- assets/js/contributions.js
+++ assets/js/contributions.js
@@ -6,8 +6,10 @@ function fetchContributions(username, callback) {
     .then(res => res.json())
     .then(data => {
       var decodedContent = atob(data.content);
-      var userData = decodedContent.split("\n\n").find(u => u.includes(username));
-      var json = userToJson(userData);
+      var userData = decodedContent
+        .split("\n\n")
+        .find(u => u.includes(username));
+      var json = yamlToJson(userData);
       callback({
         ...json,
         ordinal_rank: getNumberWithOrdinal(json.rank)
@@ -15,16 +17,16 @@ function fetchContributions(username, callback) {
     });
 }
 
-function userToJson(user) {
-  var formatted = user
-    .replace(/  - |    |'/g, '"')
-    .replace(/: /g, '": ')
-    .replace(/\n/g, ", ");
-  return JSON.parse(`{ ${formatted} }`);
+function yamlToJson(yaml) {
+  var data = yaml
+    .replace(/  - |    |"/g, "")
+    .split(/\n/g)
+    .map(x => `"${x.replace(/: /g, '":"')}"`);
+  return JSON.parse(`{ ${data.join(",")} }`);
 }
 
 function getNumberWithOrdinal(n) {
   var s = ["th", "st", "nd", "rd"];
-  var v = n % 100;
+  var v = parseInt(n) % 100;
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 }
